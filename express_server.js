4e58c00811fdb9fd8dc577668f65d182dcb4f1e8
dylanToews <dylan.toews@yahoo.ca>
2022-10-31 13:20:26 -0600

    modified express server

--- express_server.js
+++ express_server.js
@@ -15,6 +15,23 @@ app.get('/', (req, res) => {
   res.send("Happy Halloweenie");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
 app.listen(PORT, () => {
   console.log(`Example app listeneing on port ${PORT}`);
 });

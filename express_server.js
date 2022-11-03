800eb0c16c5c452e9ecff5dd5bab3ef14e63c0cc
dylanToews <dylan.toews@yahoo.ca>
2022-11-03 12:13:55 -0600

    issues with login fixed

--- express_server.js
+++ express_server.js
@@ -28,35 +28,42 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// const urlDatabase = {
+//   shortURL: {
+//     longURL : longURL,
+//     userID: userID
 
+//   }
+// }
 
 
 
 /// BREAD (GETS & POSTS)
 
 
-//Browse 
-
-// redirect 
+//GET Redirect to home page 
 
 app.get('/', (req, res) => {
   res.redirect("/urls");
 });
 
-//Browse 
+//GET Create New URL
 
 app.get("/urls/new", (req, res) => {
   const id = req.cookies.user_id;
   const user = users[id];
-
   const templateVars = {
     user,
   };
+  if (!req.cookies.user_id) {
+    res.redirect("/login");
+    return;
+  }
   res.render("urls_new", templateVars);
 });
 
 
-//Read 
+//GET - Render home/index page
 
 app.get("/urls", (req, res) => {
   const id = req.cookies.user_id;
@@ -70,31 +77,37 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//Read 
+//GET - link from short url to actual long url 
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
+  if (!urlDatabase[req.params.id]) {
+    res.send("The page you are looking for does not exist!");
+  }
   console.log(longURL);
   res.redirect(longURL);
 });
 
-//Edit existing from index page 
+//GET - go to edit url page 
 
 app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   const id = req.cookies.user_id;
   const user = users[id];
-
   const templateVars = {
     user,
     id: req.params.id,
     longURL: urlDatabase[shortURL]
   };
+  if (!req.cookies.user_id) {
+    res.redirect("/login");
+    return;
+  }
   res.render("urls_show", templateVars);
 });
 
 
-//edit
+//POST - edit existing urls 
 
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
@@ -103,33 +116,38 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(`/urls`);
 });
 
-//app.post("urls/:id", (req))
 
+// URL Datbase Object 
 
+// const urlDatabase = {
+//   shorturl: {
+//     longURL: "https://www.tsn.ca",
+//     userID: "aJ48lW",
+//   },
+//   shorturl: {
+//     longURL: "https://www.google.ca",
+//     userID: "aJ48lW",
+//   },
 
-
-// Add new URL 
+// }
+// Add new URL POST
 
 app.post("/urls", (req, res) => {
-  console.log(req.body.longURL);
+  //console.log(req.body.longURL);
   let longURL = req.body.longURL;
   let shortURL = generateRandomString();
+  if (!req.cookies.user_id) {
+    res.send("Please log in");
+    return;
+  }
+
   urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+
   res.redirect(`/urls/${shortURL}`);
 });
 
-//Login 1 
-
-// app.post("/login", (req, res) => {
-//   console.log("login req.body", req.body.username);
-//   const loginUser = req.body.username;
-
-//   res.cookie("username", loginUser);
-
-//   res.redirect("/urls");
-// });
-
-//Logout 
+//Logout POST
 
 app.post("/logout", (req, res) => {
   console.log("logout button pressed");
@@ -140,7 +158,10 @@ app.post("/logout", (req, res) => {
 //Registration GET
 
 app.get("/register", (req, res) => {
-
+  if (req.cookies.user_id) {
+    res.redirect("/urls");
+    return;
+  }
   res.render("urls_register", { user: null });
 });
 
@@ -156,39 +177,46 @@ const checkEmailForDuplicates = (enterEmail) => {
   }
 };
 
-/// USERS 
+/// Users Data Object
 
 const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "e",
+    password: "1",
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk",
+    password: "2",
   },
 };
 
-//Check email/password
+//Check email/password Function 
 
-const checkEmailPassword = (email, enterPassword) => {
+const checkEmailPassword = (email, enterPassword, users) => {
+
 
   let userKeys = Object.keys(users);
+  let myId = '';
   for (let keys of userKeys) {
+
     let allEmails = users[keys].email;
-    let myId;
     if (allEmails === email) {
       myId = keys;
     }
-    let myObj = users[myId];
-
-    if (Object.values(myObj).includes(enterPassword)) {
-      return myId;
-    }
+  }
+  if (myId === '') {
     return false;
   }
+
+  let myObj = users[myId];
+
+  if (Object.values(myObj).includes(enterPassword)) {
+    return myId;
+  }
+  return false;
+
 };
 
 
@@ -203,49 +231,50 @@ app.post("/register", (req, res) => {
     res.status(400).send("Please fill all boxes before submitting");
     return;
   }
-
   if (checkEmailForDuplicates(email)) {
     res.status(400).send("Email has already been used, please choose another email");
     return;
   }
 
-
   users[id] = { id, email, password };
+
   res.cookie("user_id", id);
-  //res.redirect("/register")
   res.redirect("/urls");
 });
 
-// Login Page
+// Login Page GET
 
 app.get("/login", (req, res) => {
-
+  if (req.cookies.user_id) {
+    res.redirect("/urls");
+    return;
+  }
   res.render("urls_login", { user: null });
 });
 
 
-// Login Post 
+// Login POST
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const id = req.body.id;
+
 
   if (checkEmailForDuplicates(email) !== true) {
     res.status(403).send("Email cannot be found");
     return;
   }
 
-  if (checkEmailPassword(email, password) === false) {
+  if (checkEmailPassword(email, password, users) === false) {
     res.status(403).send("Email does not match password");
     return;
   }
-  res.cookie("user_id", checkEmailPassword(email, password));
+  res.cookie("user_id", checkEmailPassword(email, password, users));
   res.redirect("/urls");
 });
 
 
 
-// DELETE 
+// Delete short url POST 
 
 app.post("/urls/:id/delete", (req, res) => {
   //console.log('delete button pressed');
